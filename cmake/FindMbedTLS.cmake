aaa48a6c967ba73de7b8f54b0c823f4c46e0273e
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-04-03 15:23:38 +0200

    build: add paths to find cmake

--- cmake/FindMbedTLS.cmake
+++ cmake/FindMbedTLS.cmake
@@ -1,23 +1,23 @@
-find_path(MBEDTLS_INCLUDE_DIRS mbedtls/ssl.h)
+find_path(MBEDTLS_INCLUDE_DIRS mbedtls/ssl.h PATHS "/usr/local" "/usr" "C:/Program Files (x86)/mbed TLS/include")
 
-find_library(MBEDTLS_LIBRARY mbedtls)
-find_library(MBEDX509_LIBRARY mbedx509)
-find_library(MBEDCRYPTO_LIBRARY mbedcrypto)
+find_library(MBEDTLS_LIBRARY mbedtls PATHS "C:/Program Files (x86)/mbed TLS/lib")
+find_library(MBEDX509_LIBRARY mbedx509 PATHS "C:/Program Files (x86)/mbed TLS/lib")
+find_library(MBEDCRYPTO_LIBRARY mbedcrypto PATHS "C:/Program Files (x86)/mbed TLS/lib")
 
 set(MBEDTLS_LIBRARIES "${MBEDTLS_LIBRARY}" "${MBEDX509_LIBRARY}" "${MBEDCRYPTO_LIBRARY}")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MBEDTLS DEFAULT_MSG
-    MBEDTLS_INCLUDE_DIRS MBEDTLS_LIBRARY MBEDX509_LIBRARY MBEDCRYPTO_LIBRARY)
+find_package_handle_standard_args(MBEDTLS DEFAULT_MSG MBEDTLS_INCLUDE_DIRS MBEDTLS_LIBRARY MBEDX509_LIBRARY
+                                  MBEDCRYPTO_LIBRARY)
 
 mark_as_advanced(MBEDTLS_INCLUDE_DIRS MBEDTLS_LIBRARY MBEDX509_LIBRARY MBEDCRYPTO_LIBRARY)
 
-add_library(mbedtls
-            IMPORTED
-            UNKNOWN)
-set_target_properties(mbedtls
-                      PROPERTIES IMPORTED_LOCATION
-                                 ${MBEDTLS_LIBRARIES})
-set_target_properties(mbedtls
-                      PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                 "${MBEDTLS_INCLUDE_DIRS}")
+message(STATUS "MBEDTLS_INCLUDE_DIRS: ${MBEDTLS_INCLUDE_DIRS}")
+message(STATUS "MBEDTLS_LIBRARY: ${MBEDTLS_LIBRARY}")
+message(STATUS "MBEDX509_LIBRARY: ${MBEDX509_LIBRARY}")
+message(STATUS "MBEDCRYPTO_LIBRARY: ${MBEDCRYPTO_LIBRARY}")
+message(STATUS "MBEDTLS_LIBRARIES: ${MBEDTLS_LIBRARIES}")
+
+add_library(mbedtls IMPORTED UNKNOWN)
+set_target_properties(mbedtls PROPERTIES IMPORTED_LOCATION ${MBEDTLS_LIBRARIES})
+set_target_properties(mbedtls PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${MBEDTLS_INCLUDE_DIRS}")
