4a062e1348c9bc84469b3c86efde661a2a1fc41d
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-05-11 00:59:08 +0200

    chore: only use context if needed

--- src/utils.hpp
+++ src/utils.hpp
@@ -9,9 +9,10 @@ namespace cose {
 template <typename T>
 inline std::shared_ptr<T> make_managed_array(size_t size)
 {
-	return std::shared_ptr<T>(new T[size], [](T *p) { delete[] p; });
+	return std::shared_ptr<T>(new T[size], [](T* p) { delete[] p; });
 }
 
+#ifdef USE_CBOR_CONTEXT
 template <typename T>
 inline std::shared_ptr<T> make_managed_array(size_t size,
 	cn_cbor_context* context)
@@ -19,4 +20,6 @@ inline std::shared_ptr<T> make_managed_array(size_t size,
 	return std::shared_ptr<T>(
 		COSE_CALLOC(1, size, context), [=](T* p) { COSE_FREE(p, context); });
 }
+#endif
+
 }  // namespace cose
