771da40677df3782a7a55bc9f5fa9c9171d0d6a5
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-05-11 00:22:26 +0200

    chore: use cose_free

--- src/utils.hpp
+++ src/utils.hpp
@@ -2,6 +2,8 @@
 
 #include <memory>
 
+#include "cose/cose.h"
+
 namespace cose {
 
 template <typename T>
@@ -10,4 +12,11 @@ inline std::shared_ptr<T> make_managed_array(size_t size)
 	return std::shared_ptr<T>(new T[size], [](T *p) { delete[] p; });
 }
 
+template <typename T>
+inline std::shared_ptr<T> make_managed_array(size_t size,
+	cn_cbor_context* context)
+{
+	return std::shared_ptr<T>(
+		COSE_CALLOC(1, size, context), [=](T* p) { COSE_FREE(p, context); });
+}
 }  // namespace cose
