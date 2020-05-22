0a87273a9c7ff62d34d3ada0152a9a0ba14c6944
Jim Schaad <ietf@augustcellars.com>
2020-05-22 08:30:37 -0700

    Is this compiling?

--- test/test.cpp
+++ test/test.cpp
@@ -1550,7 +1550,9 @@ int main(int argc, char** argv)
 			}
 			else if (strcmp(argv[i], "--keyFormat=native") == 0) {
 				KeyFormat = 2;
-#if defined(CMS_C_USE_OPENSSL) && (OPENSSL_VERSION_NUMBER <= 0x10100000L)
+#if defined(CMS_C_USE_OPENSSL) && (OPENSSL_VERSION_NUMBER < 0x10100000L)
+#pragma message("OPENSSL VERSION does not support native key ")
+
 				fprintf(stderr,
 					"Native key formats not supported for OpenSSL version "
 					"{%x}\n",
